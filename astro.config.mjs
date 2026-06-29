696e52042e1351aaf67e3d2eb3aca3070ce20966
badreddinexx <badreddinekx122@gmail.com>
2026-06-29 15:19:23 +0100

    fix: remove sitemap plugin to fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://britishhomeinterior.co.uk',
-  integrations: [sitemap()],
 });
